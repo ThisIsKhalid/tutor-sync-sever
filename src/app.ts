bf71c4e2658c77089e089988e81a2f9d72a6c0d0
ThisIsKhalid <iamkhalid.hs@gmail.com>
2024-09-27 14:38:36 +0600

    :art: added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -18,10 +19,23 @@ app.use(
 
 app.get("/", (req: Request, res: Response) => {
   res.send(
-    "Hello! Welcome to Tutor Sync Server. You can go to the website through this link: ",
+    `<section style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f0f0f0;">
+      <div style="text-align: center; padding: 20px; border: 1px solid #ccc; border-radius: 10px; background-color: #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+        <h1 style="color: #333; font-family: Arial, sans-serif;">Tutor-Sync Server</h1>
+        <p style="color: #666; font-family: Arial, sans-serif;">Server is running</p>
+        <p style="color: #666; font-family: Arial, sans-serif;">
+        Visit <a href="https://gitmoji.dev/" target='_blank' style="color: #007bff; text-decoration: none;">Tutor Sync</a> for more information. </p>
+      </div>
+    </section>`,
   );
 });
 
 // routes
+
+// Error Handler
+app.use(globalErrorHandler);
+
+//Not Found
+// app.use(notFound);
 
 export default app;
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,44 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import config from "../config";
+import { TErrorSources } from "../interface/error";
+
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log(err.statusCode);
+
+  // default error response
+  let statusCode = 500;
+  let message = "Something went wrong";
+  let errorSources: TErrorSources = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  // custom error response
+  if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  }
+
+  // ultimate error response
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    err,
+    stack: config.NODE_ENV === "development" ? err?.stack : null,
+  });
+};
+
+export default globalErrorHandler;
