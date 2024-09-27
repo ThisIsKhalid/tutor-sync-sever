ef3f86f4b69c8f595ed0e97e1e96da4c28c686d9
ThisIsKhalid <iamkhalid.hs@gmail.com>
2024-09-27 12:15:17 +0600

    :art: added config file, and connect with database

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import { Server } from "http";
+import mongoose from "mongoose";
 import app from "./app";
+import config from "./app/config";
 
 let server: Server;
 
 async function main() {
   try {
     // db connection
+    await mongoose.connect(config.databaseURL as string);
 
     server = app.listen(5000, () => {
-      console.log(`app is running on port 5000`);
+      console.log(`app is running on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
